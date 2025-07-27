4de86c8791eadd9144e541e416899fc4b7def594
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2025-07-27 17:45:29 -0300

    incluir tarefa concluída

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,21 +1,47 @@
 function preencherListas(todoList, doneList) {
-    todoList.innerHTML = "";
-    doneList.innerHTML = "";
+    zerarListasElemento(todoList, doneList);
     let tarefas = getTarefas();
+    if(tarefas.length != 0){
     tarefas.forEach((tarefa) => {
         let li = document.createElement("li");
         li.textContent = tarefa.tarefa;
-        li.id = tarefa.tarefa;
+        li.id = tarefa.tarefa.replace(" ","-");
+        li.addEventListener("click", (event) => {
+            concluirTarefa(event.target.textContent, todoList, doneList);
+        });
         if (tarefa.status === "pendente") {
             todoList.appendChild(li);
         } else {
             doneList.appendChild(li);
         }
     });
-    console.log("foi");
+    }
 }
 
-function setTarefa(tarefa) {
+function zerarListasElemento(todoList, doneList){
+    todoList.innerHTML = "";
+    doneList.innerHTML = "";
+}
+
+function concluirTarefa(tarefaConcluida, todoList, doneList){
+    let tarefas = getTarefas();
+    tarefas.forEach((tarefa) => {
+        if(tarefa.tarefa === tarefaConcluida){
+            tarefa.status = 'concluída';
+        }
+    });
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    preencherListas(todoList, doneList);
+}
+
+function insert(input, todoList, doneList){
+    let tarefa = input.value;
+    setTarefaLocalStorage(tarefa);
+    preencherListas(todoList, doneList);
+    input.value = "";
+}
+
+function setTarefaLocalStorage(tarefa) {
     if (tarefa) {
         let tarefas = getTarefas();
         tarefas.push({ tarefa, status: "pendente" });
@@ -31,4 +57,4 @@ function getTarefas() {
     return tarefas;
 }
 
-export { setTarefa, preencherListas };
+export { setTarefaLocalStorage, preencherListas, insert};
